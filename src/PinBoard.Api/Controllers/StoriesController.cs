36078c1d5703d517822ae26d406c8775112ec478
MatKrasuski <mat.krasuski@gmail.com>
2019-04-18 22:07:42 +0200

    tests added

---
+++ src/PinBoard.Api/Controllers/StoriesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PinBoard.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoriesController : ControllerBase
+    {
+        [HttpGet]
+        public string GetAllStories()
+        {
+            return "json result";
+        }
+    }
+}
---
+++ tests/PinBoard.Unit.Tests/Controller/StoriesControllerTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace PinBoard.Unit.Tests.Controller
+{
+    public class StoriesControllerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
